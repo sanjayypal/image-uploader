5ced418258e663c221d8c7f0977cc8afdfa65631
sanjayypal <sanjayypal113@gmail.com>
2024-10-09 17:59:14 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            console.log("bucket id : ",import.meta.env.VITE_APPWRITE_URL)
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            const data = await this.account.get();
+            return data;
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+            return false;
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
+
